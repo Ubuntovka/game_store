39c6a80daf785a8dae92893aeff1ecc1728250c1
Mariia Katsala <mariia_katsala@epam.com>
2022-01-17 12:43:34 +0200

    New roles

--- gs_app/database/create_users.py
+++ gs_app/database/create_users.py
@@ -5,11 +5,21 @@ from gs_app import user_datastore
 def create_user():
     manager_role = user_datastore.find_or_create_role(name='manager', description='Manager role')
     user = user_datastore.create_user(
+        username='manager',
         email='manager@example.com',
         password=generate_password_hash('asdf1234').decode('utf8'),
         roles=[manager_role]
     )
     user_datastore.add_role_to_user(user, manager_role)
+
+    admin_role = user_datastore.find_or_create_role(name='admin', description='Admin role')
+    user = user_datastore.create_user(
+        username='admin',
+        email='admin@example.com',
+        password=generate_password_hash('zxcv1234').decode('utf8'),
+        roles=[admin_role]
+    )
+    user_datastore.add_role_to_user(user, admin_role)
 
 
 if __name__ == '__main__':
